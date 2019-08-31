890c8bd6c5d78f42ade39b0ecda16c9dbe40219b
Ubuntu <ubuntu@ip-172-31-46-169.ec2.internal>
2019-08-31 21:08:33 +0000

    first commit

---
+++ todo/test_app.py
@@ -0,0 +1,8 @@
+from django.apps import apps
+from django.test import TestCase
+from .app import TodoConfig
+
+class TestToDoConfig(TestCase):
+    def test_app(self):
+        self.assertEqual("todo", TodoConfig.name)
+        self.assertEqual("todo", apps.get_app_config("todo").name)
